7ce5ef0274e5658973a105d6edd61d7e53c57328
Nikita Koniukh <nikita@msapps>
2020-05-25 16:43:41 +0300

    Initial Commit

---
+++ MSAppsWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MSAppsWebView
+//
+//  Created by Nikita Koniukh on 25/05/2020.
+//  Copyright © 2020 Nikita Koniukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
